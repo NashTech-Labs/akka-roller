043221deb69968aa95d06105b93417407e38fec1
Vikas Hazrati <vikas@knoldus.com>
2014-01-17 08:25:48 +0530

    Day two clustering code

--- build.sbt
+++ build.sbt
@@ -8,6 +8,7 @@ libraryDependencies ++= Seq(
   "com.typesafe.akka"	%%	"akka-actor"		%	"2.2.3",
   "com.typesafe.akka" 	%% 	"akka-testkit" 		% 	"2.2.3",
   "com.typesafe.akka" 	%%	"akka-remote" 		% 	"2.2.3",
+  "com.typesafe.akka" 	%% 	"akka-cluster" 		% 	"2.2.3",
   "org.scalatest" 		%% 	"scalatest" 		% 	"2.0" 	% 	"test",
   "junit" 				% 	"junit" 			% 	"4.11" 	% 	"test",
   "com.novocode" 		% 	"junit-interface"	% 	"0.10" 	% 	"test"
---
+++ src/main/scala/com/knoldus/akka/clustering/Worker.scala
@@ -0,0 +1,56 @@
+package com.knoldus.akka.clustering
+
+import language.postfixOps
+import scala.concurrent.duration._
+import akka.actor.Actor
+import akka.actor.ActorRef
+import akka.actor.ActorSystem
+import akka.actor.Props
+import akka.actor.RootActorPath
+import akka.actor.Terminated
+import akka.cluster.Cluster
+import akka.cluster.ClusterEvent.CurrentClusterState
+import akka.cluster.ClusterEvent.MemberUp
+import akka.cluster.Member
+import akka.cluster.MemberStatus
+import akka.pattern.ask
+import akka.util.Timeout
+import com.typesafe.config.ConfigFactory
+
+object TransformationBackend {
+  def main(args: Array[String]): Unit = {
+    // Override the configuration of the port when specified as program argument
+    val config =
+      (if (args.nonEmpty) ConfigFactory.parseString(s"akka.remote.netty.tcp.port=${args(0)}")
+      else ConfigFactory.empty).withFallback(
+        ConfigFactory.parseString("akka.cluster.roles = [backend]")).
+        withFallback(ConfigFactory.load())
+
+    val system = ActorSystem("ClusterSystem", config)
+    system.actorOf(Props[TransformationBackend], name = "backend")
+  }
+}
+
+//#backend
+class TransformationBackend extends Actor {
+
+  val cluster = Cluster(context.system)
+
+  // subscribe to cluster changes, MemberUp
+  // re-subscribe when restart
+  override def preStart(): Unit = cluster.subscribe(self, classOf[MemberUp])
+  override def postStop(): Unit = cluster.unsubscribe(self)
+
+  def receive = {
+    case TransformationJob(text) ⇒ println("Job received");sender ! TransformationResult(text.toUpperCase)
+    case state: CurrentClusterState ⇒
+      state.members.filter(_.status == MemberStatus.Up) foreach register
+    case MemberUp(m) ⇒ register(m)
+  }
+
+  def register(member: Member): Unit =
+    if (member.hasRole("frontend"))
+      context.actorSelection(RootActorPath(member.address) / "user" / "frontend") !
+        BackendRegistration
+}
+//#backend
