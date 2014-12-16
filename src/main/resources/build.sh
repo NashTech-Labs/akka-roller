c8929e26aeda5b5236267093439625d8e28a55ca
Vikas Hazrati <vikas@knoldus.com>
2014-12-16 13:38:53 +0530

    Clean buildbox build file

--- src/main/resources/build.sh
+++ src/main/resources/build.sh
@@ -3,11 +3,11 @@ function run() {
   echo -e "\e[05;34m$1\e[0m" # blue text
   time $1
 }
-# Maven cleans better than SBT
-# run "mvn clean"
+
 run "sbt clean"
 run "sbt compile"
-run "sbt universal:packageBin"
-run "sbt universal:dist"
+run "sbt packageBin"
+run "sbt dist"
+
 TODAY=`date +"%F-%H-%M-%S"`
 git log -n 1 --pretty="[%an] %cD %s" > akka-roller-build.txt
