f3b74aa2609b68a53a5d3dca72be990c8484c87b
Vikas Hazrati <vikas@knoldus.com>
2015-10-19 19:16:10 +0200

    Example starting implementation for Parking Lot

---
+++ src/main/scala/com/knoldus/akka/actor/ParkingLot.scala
@@ -0,0 +1,51 @@
+package com.knoldus.akka.actor
+
+
+import akka.actor.{Actor, ActorSystem, Props}
+import akka.pattern.ask
+import akka.util.Timeout
+
+import scala.concurrent.Await
+import scala.concurrent.duration._
+
+
+object Init {
+  val system = ActorSystem("PL")
+  val driver = system.actorOf(Props[Driver], "Driver")
+  val slotMonitor = system.actorOf(Props[SlotMonitor], "SlotMonitor")
+  val attendant = system.actorOf(Props[Attendant], "Attendant")
+}
+
+object StartParking extends App{
+  Init.driver ! "Request Parking"
+}
+
+
+class Driver extends Actor{
+  def receive ={
+    case "Request Parking" => Init.attendant ! "LetMePark"
+    case _ => "Driver"
+  }
+}
+
+class SlotMonitor extends Actor {
+  def receive ={
+    case "GiveMeEmptySlot" => sender ! 5
+    case _ => "Slot Monitor"
+  }
+}
+
+class Attendant extends Actor {
+  implicit val timeout = Timeout(5.seconds)
+  def receive ={
+    case "LetMePark" => {
+      println("Got the message to park")
+      val parkingSlot = Await.result((Init.slotMonitor ? "GiveMeEmptySlot").mapTo[Int], 5.seconds)
+      println(s"parking at ${parkingSlot}")
+    }
+    case _ => "Attendant"
+  }
+}
+
+
+
--- src/main/scala/com/knoldus/akka/health/Doctor.scala
+++ src/main/scala/com/knoldus/akka/health/Doctor.scala
@@ -1,13 +1,11 @@
 package com.knoldus.akka.health
 
-import akka.actor.ActorSystem
-import akka.util.Timeout
-import akka.actor.Props
-import akka.util.Timeout
-import scala.concurrent.duration._
-import scala.concurrent.Await
-import akka.actor.ActorRef
+import akka.actor.{ActorRef, ActorSystem, Props}
 import akka.pattern.ask
+import akka.util.Timeout
+
+import scala.concurrent.Await
+import scala.concurrent.duration._
 
 // The futures created by the ask syntax need an
 // execution context on which to run, and we will use the
@@ -21,13 +19,13 @@ object Doctor extends App {
   val theatre = system.actorOf(Props[OperationTheater], "OperationTheatre")
 
   // Grab the controls
-  val control = Await.result((theatre ? OperationTheater.PassMeTheMachine).mapTo[ActorRef], 5.seconds)
-  system.scheduler.scheduleOnce(200.millis) { control ! HeartLungMachine.PullBack(1f) }
-  system.scheduler.scheduleOnce(1.seconds) { control ! HeartLungMachine.PullBack(0f) }
-  system.scheduler.scheduleOnce(3.seconds) { control ! HeartLungMachine.PullBack(0.5f) }
-  system.scheduler.scheduleOnce(4.seconds) { control ! HeartLungMachine.PullBack(0f) }
+  val heartLungMachine = Await.result((theatre ? OperationTheater.PassMeTheMachine).mapTo[ActorRef], 5.seconds)
+  system.scheduler.scheduleOnce(200.millis) { heartLungMachine ! HeartLungMachine.PullBack(1f) }
+  system.scheduler.scheduleOnce(1.seconds) { heartLungMachine ! HeartLungMachine.PullBack(0f) }
+  system.scheduler.scheduleOnce(3.seconds) { heartLungMachine ! HeartLungMachine.PullBack(0.5f) }
+  system.scheduler.scheduleOnce(4.seconds) { heartLungMachine ! HeartLungMachine.PullBack(0f) }
 
   // Shut down
-  system.scheduler.scheduleOnce(5.seconds) { system.shutdown() }
+  system.scheduler.scheduleOnce(5.seconds) { system.terminate() }
 
 }
