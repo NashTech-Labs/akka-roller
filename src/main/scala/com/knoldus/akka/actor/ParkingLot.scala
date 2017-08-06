9253dadc58fc5e9b1cfc0c94106a9a18e3989b05
sidharth <sidharth.khattri@gmail.com>
2017-08-06 15:35:44 +0530

    reformatted parking lot

--- src/main/scala/com/knoldus/akka/actor/ParkingLot.scala
+++ src/main/scala/com/knoldus/akka/actor/ParkingLot.scala
@@ -1,6 +1,5 @@
 package com.knoldus.akka.actor
 
-
 import akka.actor.{Actor, ActorSystem, Props}
 import akka.pattern.ask
 import akka.util.Timeout
@@ -8,9 +7,9 @@ import akka.util.Timeout
 import scala.concurrent.Await
 import scala.concurrent.duration._
 
-
 object Init {
   val system = ActorSystem("PL")
+
   val driver = system.actorOf(Props[Driver], "Driver")
   val slotMonitor = system.actorOf(Props[SlotMonitor], "SlotMonitor")
   val attendant = system.actorOf(Props[Attendant], "Attendant")
@@ -20,18 +19,17 @@ object StartParking extends App {
   Init.driver ! "Request Parking"
 }
 
-
 class Driver extends Actor {
   def receive = {
     case "Request Parking" => Init.attendant ! "LetMePark"
-    case x => println("Driver")
+    case x                 => println("Driver")
   }
 }
 
 class SlotMonitor extends Actor {
   def receive = {
     case "GiveMeEmptySlot" => sender ! 5
-    case x => println("Slot Monitor")
+    case msg               => println("Slot Monitor")
   }
 }
 
@@ -39,14 +37,10 @@ class Attendant extends Actor {
   implicit val timeout = Timeout(5.seconds)
 
   def receive = {
-    case "LetMePark" => {
+    case "LetMePark" =>
       println("Got the message to park")
       val parkingSlot = Await.result((Init.slotMonitor ? "GiveMeEmptySlot").mapTo[Int], 5.seconds)
-      println(s"parking at ${parkingSlot}")
-    }
-    case x => println("Attendant")
+      println(s"parking at $parkingSlot")
+    case msg         => println("Attendant")
   }
 }
-
-
-
