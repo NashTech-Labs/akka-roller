c653fa385eb4cf174afe40249285b8ad295f4f0d
sidharth <sidharth.khattri@gmail.com>
2017-08-06 16:52:58 +0530

    reformatted code

--- src/main/scala/com/knoldus/akka/actor/StopActing.scala
+++ src/main/scala/com/knoldus/akka/actor/StopActing.scala
@@ -1,25 +1,28 @@
 package com.knoldus.akka.actor
 
-import akka.actor.{Props, ActorSystem, Actor}
+import akka.actor.{Actor, ActorSystem, Props}
 
-object StopActing extends App{
- val system = ActorSystem("Now")
+object StopActing extends App {
+  val system = ActorSystem("Now")
+
   val show = system.actorOf(Props(classOf[ShowStopper]))
+
   show ! "hey"
   show ! "hey again"
+
   system stop show
+
   show ! "hey yet again"
+
   system terminate()
-
-
 }
 
 class ShowStopper extends Actor {
-  def receive = {
-    case x => println(s" I process almost anything! like - ${x} ")
+  override def postStop() = {
+    println(" Aah! , I would seek revenge!")
   }
 
-  override def postStop() = {
-    println (" Aah! , I would seek revenge!")
+  def receive = {
+    case msg => println(s" I process almost anything! like - $msg")
   }
 }
